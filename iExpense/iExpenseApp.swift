9a10c0c0c20482d2b34144864cff838941aeccd4
Berserk-21 <cryptogekkofr@protonmail.com>
2024-05-14 10:04:23 +0200

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Berserk on 14/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
