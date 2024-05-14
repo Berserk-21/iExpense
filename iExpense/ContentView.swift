f3f6c11ca7b50235d7e94ad6beff997f1caf3766
Berserk-21 <cryptogekkofr@protonmail.com>
2024-05-14 10:30:24 +0200

    Learn @Observable class

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -6,17 +6,27 @@
 //
 
 import SwiftUI
+import Observation
 
 struct ContentView: View {
+    
+    @State private var user = User()
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Your name is \(user.firstName) \(user.lastName).")
+            
+            TextField("FirstName", text: $user.firstName)
+            TextField("LastName", text: $user.lastName)
         }
         .padding()
     }
+}
+
+// A class has to be observable to allow the view to watch changes of objects from this class.
+@Observable
+class User {
+    var firstName = "Bilbo"
+    var lastName = "Baggins"
 }
 
 #Preview {
